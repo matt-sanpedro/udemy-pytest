d0025732b41ec388ae566975425c6101ebb94311
mobius <mattsanpedro7@gmail.com>
2023-11-08 20:14:49 -0800

    Added fixture code samples

--- s-01-pytest-framework/project/pytest_topics/py_assertions/test_fixture01.py
+++ s-01-pytest-framework/project/pytest_topics/py_assertions/test_fixture01.py
@@ -6,8 +6,18 @@ import pytest
 @pytest.fixture()
 def setup_list():
     print("\n in fixtures.. \n")
-    city = ["Los Angeles", "Greenville", "Manila", "New York"]
+    city = ["Los Angeles", "Greenville", "Manila", "New York", "Mumbai"]
     return city
 
 def test_getItem(setup_list):
-    pass
+    print(setup_list)
+    assert setup_list[0] == "Los Angeles"
+    assert setup_list[::2] == ["Los Angeles", "Manila", "Mumbai"]
+
+def myReverse(my_list):
+    my_list.reverse()
+    return my_list
+
+def test_reverseList(setup_list):
+    assert setup_list[::-2] == ["Mumbai", "Manila", "Los Angeles"]
+    assert setup_list[::-1] == myReverse(setup_list)
